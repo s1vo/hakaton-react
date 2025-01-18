2e4440b3d3fcf768d94e6c97f7baa4c304126906
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-01-18 18:52:40 +0300

    move progressData to constant file

--- src/pages/participants/participantsPages.jsx
+++ src/pages/participants/participantsPages.jsx
@@ -1,34 +1,14 @@
-import React from 'react';
-import {ProgressBar} from "../../shared/ui/";
-
-const progressData = [
-    {
-        label: "HTML",
-        value: 25,
-        color: "red",
-    },
-    {
-        label: "JavaScript",
-        value: 50,
-        color: "orange",
-    },
-    {
-        label: "CSS",
-        value: 30,
-        color: "blue",
-    },
-];
-
+import React from "react";
+import { ProgressBar } from "../../shared/ui/";
+import { progressData } from "./constants";
 
 export const ParticipantsPages = () => {
-    return (
-        <div>
-            <h2>Practicipants</h2>
-            <div className="progressBar">
-                <ProgressBar type={"polar"} props={progressData}/>
-            </div>
-        </div>
-    )
-}
-
-
+  return (
+    <div>
+      <h2>Practicipants</h2>
+      <div className="progressBar">
+        <ProgressBar type={"polar"} props={progressData} />
+      </div>
+    </div>
+  );
+};
