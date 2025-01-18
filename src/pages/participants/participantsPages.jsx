4f3eeea97bb1f2667631d3a9790bc0791847f73a
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-01-18 19:10:27 +0300

    add selector type diagram for progressBar

--- src/pages/participants/participantsPages.jsx
+++ src/pages/participants/participantsPages.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { ProgressBar } from "../../shared/ui/";
+import { ProgressBarContainer } from "../../shared/ui/";
 import { progressData } from "./constants";
 
 export const ParticipantsPages = () => {
   return (
     <div>
       <h2>Practicipants</h2>
-      <div className="progressBar">
-        <ProgressBar type={"polar"} props={progressData} />
-      </div>
+      <ProgressBarContainer props={progressData} />
     </div>
   );
 };
