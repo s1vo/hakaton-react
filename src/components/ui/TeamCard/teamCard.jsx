3f52300f17be3164f63199b83433b30b5de1c9a8
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-01-19 16:19:45 +0300

    Исправления страницы участника

--- src/components/ui/TeamCard/teamCard.jsx
+++ src/components/ui/TeamCard/teamCard.jsx
@@ -1,49 +1,30 @@
-import React from 'react';
-import './teamCard.css';
-import photoMisha from '../../../assets/images/avatars/misha.jpg';
-import photoRuslan from '../../../assets/images/avatars/ruslan.jpg';
-import photoNikita from '../../../assets/images/avatars/nikita.jpg';
-
-// Пример данных участников команды
-const teamMembers = [
-    {
-        id: 1,
-        name: 'Сивоконь Михаил',
-        role: 'Team lead разработки',
-        photo: photoMisha, // Замените на URL фотографии
-        description: 'Михаил отвечает за управление командой, распределение задач и обеспечение высокого качества разработки. Его опыт и лидерские качества помогают команде эффективно достигать поставленных целей.',
-    },
-    {
-        id: 2,
-        name: 'Мамедбеков Руслан',
-        role: 'Разработчик Frontend',
-        photo: photoRuslan, // Замените на URL фотографии
-        description: 'Руслан занимается разработкой пользовательского интерфейса, внедрением новых технологий и созданием интерактивных и удобных веб-приложений.',
-    },
-    {
-        id: 3,
-        name: 'Никита Рябиков',
-        role: 'Разработчик Frontend',
-        photo: photoNikita, // Замените на URL фотографии
-        description: 'Никита специализируется на создании адаптивных дизайнов и оптимизации фронтенд-кода для обеспечения быстродействия и удобства использования.',
-    },
-];
+import React from "react";
+import "./teamCard.css";
+import { useNavigate } from "react-router-dom";
+import { teamMembers } from "../../../DB/members";
 
 export const TeamCard = () => {
-    return (
-        <section className="team">
-            <h2 className="team-title">Наша команда</h2>
-            <div className="team-container">
-                {teamMembers.map((member) => (
-                    <div key={member.id} className="team-card">
-                        <img src={member.photo} alt={member.name} className="team-photo" />
-                        <h3 className="team-name">{member.name}</h3>
-                        <p className="team-role">{member.role}</p>
-                        <p className="team-description">{member.description}</p>
-                        <button className="team-button">Подробнее</button>
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <section className="team">
+      <h2 className="team-title">Наша команда</h2>
+      <div className="team-container">
+        {teamMembers.map((member) => (
+          <div key={member.id} className="team-card">
+            <img src={member.photo} alt={member.name} className="team-photo" />
+            <h3 className="team-name">{member.name}</h3>
+            <p className="team-role">{member.role}</p>
+            <p className="team-description">{member.description}</p>
+            <button
+              className="team-button"
+              onClick={() => navigate(`/participant/${member.id}`)}
+            >
+              Подробнее
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
--- src/pages/participants/components/user-image-container/user-imgae-container.jsx
+++ src/pages/participants/components/user-image-container/user-imgae-container.jsx
@@ -1,24 +1,35 @@
 import React from "react";
-import userImg from "../../../../assets/img/user/8b167af653c2399dd93b952a48740620.jpg";
 import styles from "./user-image-container.module.scss";
 
-export const UserImage = () => {
+export const UserImage = ({ userId, teamMembers }) => {
   return (
     <div className={styles.userImgContainer}>
-      <img className={styles.userImg} src={userImg} />
+      <img className={styles.userImg} src={teamMembers[userId].photo} />
       <div className={styles.personalData}>
         <ul>
           <li>
-            <h3>Уровень Уровень Уровень</h3>
+            <h3>{teamMembers[userId].name}</h3>
           </li>
           <li>
-            <h3>100 лет</h3>
+            <h3>Возраст: {teamMembers[userId].age}</h3>
           </li>
         </ul>
         <ul className={styles.socialMediaContainer}>
-          <li>VK</li>
-          <li>Telega</li>
-          <li>Запрещенка</li>
+          <li>
+            <a href="https://vk.com" target="_blank">
+              ВК
+            </a>
+          </li>
+          <li>
+            <a href="https://web.telegram.org/" target="_blank">
+              Телеграм
+            </a>
+          </li>
+          <li>
+            <a href="https://rkn.gov.ru/" target="_blank">
+              Запрещенка
+            </a>
+          </li>
         </ul>
       </div>
     </div>
