fea4e7e214a101525142143c1514c2e69aa30810
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-01-19 10:28:03 +0300

    add navigate to main page with click on logo in header

--- src/shared/layout/navbar/navbar.js
+++ src/shared/layout/navbar/navbar.js
@@ -1,54 +1,41 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom'; // Используем NavLink вместо Link
-import './navbar.css';
-import logo from '../../../assets/images/logo.svg';
+import React from "react";
+import { Link, NavLink } from "react-router-dom"; // Используем NavLink вместо Link
+import logo from "../../../assets/images/logo.svg";
+import "./navbar.css";
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="Логотип" width="45" height="45" />
-                <span className="navbar-logo-text">Хакатон React</span>
-            </div>
-            <ul className="navbar-links">
-                <li>
-                    <NavLink
-                        className="navbar-link"
-                        to="/"
-                        exact
-                        activeClassName="active"
-                    >
-                        Главная
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className="navbar-link"
-                        to="/about"
-                        activeClassName="active"
-                    >
-                        О нас
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className="navbar-link"
-                        to="/participants"
-                        activeClassName="active"
-                    >
-                        Участники
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className="navbar-link"
-                        to="/contacts"
-                        activeClassName="active"
-                    >
-                        Контакты
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logo} alt="Логотип" width="45" height="45" />
+        </Link>
+        <Link to="/">
+          <span className="navbar-logo-text">Хакатон React</span>
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <NavLink className="navbar-link" to="/" exact activeClassName="active">
+            Главная
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navbar-link" to="/about" activeClassName="active">
+            О нас
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navbar-link" to="/participants" activeClassName="active">
+            Участники
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navbar-link" to="/contacts" activeClassName="active">
+            Контакты
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
